4fb4eea51a8212d51031345be128434c74561092
NattapolChan <66078943+NattapolChan@users.noreply.github.com>
2021-06-29 10:11:45 +0700

    check coordinate
    check with coding train logo

--- sketch.js
+++ sketch.js
@@ -8,23 +8,18 @@ let ay = [];
 let by = [];
 let f_x = [];
 let f_y = [];
-const N = 100;
-const period = 200 * Math.PI;
-const T = Math.floor(2*Math.PI*100);
+let temp = [];
+const N = 80;
+const T = Math.floor(2*Math.PI*element.length/10);
+const w = element.length/10
 let original_graph_x = [];
 let original_graph_y = [];
 function setup(){
-	createCanvas(700, 700);
+	createCanvas(900, 900);
 	background(255);
 	for(i=0;i<T;i++){
-		if(i<T/2){
-			original_graph_x[i] = 40;
-			original_graph_y[i] = 40;
-		}
-		else{
-			original_graph_x[i] = 0;
-			original_graph_y[i] = 0;
-		}
+		original_graph_x[i] = element[floor(element.length*i/T)].y/2;
+		original_graph_y[i] = element[floor(element.length*i/T)].x/2;
 	}
 	for(i=0;i<T;i++){
 		ax[i] = 0;
@@ -56,7 +51,6 @@ function draw(){
 	FT_x();
 	FT_y();
 	stroke(200,200,200);
-	beginShape();
 	let cumu_x = 0;
 	let cumu_y = 0;
 
@@ -78,40 +72,57 @@ function draw(){
 
 	f_x.unshift(cumu_x);
 	f_y.unshift(cumu_y);
-	if(f_x.length > 600){
+	if(f_x.length > 3000){
 		f_x.pop();
 		f_y.pop();
 	}
-	
-	for(i=0;i<f_x.length; i++){
-		vertex(i+200, 300 + f_x[i]);
-	}
 
-	endShape(OPEN);
+	// beginShape();
+	// for(i=0;i<f_x.length; i++){
+	// 	vertex(i+200, 300 + f_x[i]);
+	// }
+	// endShape(OPEN);
 
-	beginShape();
-	for(i=0;i<f_y.length; i++){
-		vertex(400 + f_y[i], i+30);
-	}
-	endShape(OPEN);
+	// beginShape();
+	// for(i=0;i<f_y.length; i++){
+	// 	vertex(300 + f_y[i], i+200);
+	// }
+	// endShape(OPEN);
 
 	// circle along y axis 
 	xpointer_x = 100;
 	ypointer_x = 380;
 	for(i=1;i<N;i++){
 		ellipse(xpointer_x, ypointer_x, 2*ax[i])
-		ypointer_x += ax[i] * cos(colortime*i/200);
-		xpointer_x += ax[i] * sin(colortime*i/200);
+		ypointer_x += ax[i] * cos(colortime*i*Math.PI/T);
+		xpointer_x += ax[i] * sin(colortime*i*Math.PI/T);
 		ellipse(xpointer_x, ypointer_x, 2* bx[i])
-		ypointer_x += bx[i] * sin(colortime*i/200);
-		xpointer_x += bx[i] * cos(colortime*i/200);
+		ypointer_x += bx[i] * sin(colortime*i*Math.PI/T);
+		xpointer_x += bx[i] * cos(colortime*i*Math.PI/T);
 	}
-	//line(xpointer_x,ypointer_x,200,100+f_x[0]);
 
 	// circle along x axis
-	xpointer_y = 400;
+	xpointer_y = 380;
 	ypointer_y = 100;
+	for(i=1;i<N;i++){
+		ellipse(xpointer_y, ypointer_y, 2* ay[i])
+		ypointer_y += ay[i] * sin(colortime*i*Math.PI/T);
+		xpointer_y += ay[i] * cos(colortime*i*Math.PI/T);
+		ellipse(xpointer_y, ypointer_y, 2* by[i])
+		ypointer_y += by[i] * cos(colortime*i*Math.PI/T);
+		xpointer_y += by[i] * sin(colortime*i*Math.PI/T);
+	}
 
+	line(xpointer_x, ypointer_x, xpointer_y, ypointer_x);
+	line(xpointer_y, ypointer_y, xpointer_y, ypointer_x);
+
+	ellipse(xpointer_y, ypointer_x, 10);
+
+	beginShape();
+	for(i=0;i<f_x.length;i++){
+		vertex(285+f_y[i], 380+f_x[i]);
+	}
+	endShape(OPEN);
 
 }
 
